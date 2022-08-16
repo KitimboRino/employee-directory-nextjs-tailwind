aa7b8e8a88135f2f5f7d8d00fd346226f2eec7c3
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2022-08-17 01:29:15 +0300

    feat(ThemeSwitch): add theme dark and light mode switch

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+// import '../styles/globals.css'
+import { ThemeProvider } from 'next-themes'
+import TopBar from '../components/TopBar'
+
+import 'tailwindcss/tailwind.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    // attribute = 'class' will set a 'dark' css class on the main <html> tag
+    <ThemeProvider attribute='class'>
+      <div
+        className='dark:bg-gray-900 bg-gray-50 w-full min-h-screen'
+      >
+        <TopBar />
+        <Component {...pageProps} />
+      </div>
+    </ThemeProvider>
+
+  )
 }
 
 export default MyApp
